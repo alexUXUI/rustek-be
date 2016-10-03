552b1b5aaa3828b8c7069b31080dc3928c161c76
Alex Bennett <alexandergraybennett@gmail.com>
2016-10-02 20:53:45 -0600

    adds email

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var dotenv = require('dotenv')
+dotenv.load()
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var signup = require('./routes/signup');
 
 var app = express();
+
+var knex = require('knex')({
+  client: 'pg',
+  // connection: 'postgres://dkkrmpowshaegz:1px7fDlATLW8UH7im7WOlGrWyh@ec2-54-243-203-143.compute-1.amazonaws.com:5432/d7o4uvt2ijj2o1?ssl=true'
+  connection: process.env.DB_CONNECTION
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +33,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/signup', signup);
+
+app.get('/test', (req, res, next) => {
+  res.send('hello!')
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -33,7 +47,6 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
@@ -55,6 +68,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
---
+++ routes/signup.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var nodemailer = require('nodemailer');
+var mg = require('nodemailer-mailgun-transport');
+var dotenv = require('dotenv');
+var router = express.Router();
+
+dotenv.load();
+
+router.post('/', function(req, res, next) {
+
+  var name = req.body.name
+  var email = req.body.email
+  console.log(`name: ${ name  }, email ${email}`)
+
+  var auth = {
+    auth: {
+      api_key: process.env.MAILGUN_KEY,
+      domain: process.env.MAILGUN_DOMAIN
+    }
+  }
+
+  var nodemailerMailgun = nodemailer.createTransport(mg(auth));
+
+  nodemailerMailgun.sendMail({
+    from: 'alexandergraybennett@gmail.com',
+    to: `${ email }`,
+    subject: `Hey ${ name }`,
+    html: `We <b>greatly</b> appreciate you signing up!
+    <br />
+    -- Rustek Team
+    `,
+  }, function (err, info) {
+    if (err) console.log('Error: ' + err);
+    else console.log('Response: ' + info);
+  });
+
+  res.render('index', { title: 'Email Sent' });
+
+});
+
+module.exports = router;
